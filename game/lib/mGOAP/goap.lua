f2b74cd6e94f515ca73a88ff6c96dd408c6a4dbd
Meowybot <jorgedidier149@gmail.com>
2026-04-18 09:42:50 -0600

    add states actions and goals to the new function

--- game/lib/mGOAP/goap.lua
+++ game/lib/mGOAP/goap.lua
@@ -6,7 +6,12 @@ gomt.__index = gomt
 function gomt:setGoal()
 end
 
-function goap.new()
+function goap.new(statess, actionss, goalss)
+  local newobj = setmetatable({
+    states = (statess or {}),
+    actions = (actionss or {}),
+    goals = (goalss or {})
+  }, gomt)
 end
 
 return goap
