16cda77e7b8ccf161aadc111a3497a363a6ea0b4
Meowybot <jorgedidier149@gmail.com>
2026-04-18 14:45:11 -0600

    Update goap.lua

--- game/lib/mGOAP/goap.lua
+++ game/lib/mGOAP/goap.lua
@@ -1,18 +1,43 @@
 local astar = require("lib.astar.astar")
 local goap = {}
 local gomt = {}
-local goas = astar
+local goas = astar --[[ not supposed to
+be astar library, supposed to be the
+astar ai thing but not yet
+--]]
 gomt.__index = gomt
 
-local function shallow_copy(t)
+goap.customtypes = {}
+
+local function deep_copy(t)
   local t2 = {}
   for i, v in pairs(t) do
-    t2[i] = v
+    if type(v) ~= "table" then
+      t2[i] = v
+    else
+      t2[i] = deep_copy(v)
+    end
   end
   return t2
 end
 
-function gomt:setGoal()
+function goap.addType(name, ...)
+  goap.customtypes[name] = {}
+  for _, v in ipairs({...}) do
+    goap.customtypes[name][v] = true
+  end
+end
+
+function goap.addPreset(nameot, ...)
+  for _, v in ipairs({...}) do
+    goap.customtypes[name][v] = true
+  end
+end
+
+function goap.delPreset(nameot, ...)
+  for _, v in ipairs({...}) do
+    goap.customtypes[name][v] = nil
+  end
 end
 
 function goap.new(statess, actionss, goalss)
