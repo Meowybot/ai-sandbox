9ecfb422deb3ad0f20a664dd31a522c66d5c9e2c
Meowybot <jorgedidier149@gmail.com>
2026-04-18 11:16:15 -0600

    Update goap.lua

--- game/lib/mGOAP/goap.lua
+++ game/lib/mGOAP/goap.lua
@@ -1,6 +1,7 @@
 local astar = require("lib.astar.astar")
 local goap = {}
 local gomt = {}
+local goas = astar
 gomt.__index = gomt
 
 function gomt:setGoal()
@@ -12,6 +13,14 @@ function goap.new(statess, actionss, goalss)
     actions = (actionss or {}),
     goals = (goalss or {})
   }, gomt)
+  newobj.w = {}
+  for i, v in pairs(actions) do
+    newobj.w[i] = {}
+    for j, _ in pairs(v.p) do
+      newobj.w[i][j] = 1
+    end
+  end
+  return newobj
 end
 
 return goap
